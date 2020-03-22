f5b35752027bd4339cf3e763a7ee0b9d3ee97f3f
Sergio <sergiocabral.1990@gmail.com>
2020-03-22 02:03:07 -0300

    Se agrega Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@
  */
 
 require('./bootstrap');
+import Vuetify from 'vuetify';
 
 window.Vue = require('vue');
 
@@ -13,13 +14,14 @@ window.Vue = require('vue');
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * Eg. ./components/HomeComponent.vue -> <example-component></example-component>
  */
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.use(Vuetify);
+Vue.component('home-component', require('./components/HomeComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +31,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
 });
+
---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,159 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer
+            v-model="drawer"
+            :clipped="$vuetify.breakpoint.lgAndUp"
+            app
+        >
+            <v-list dense>
+                <template v-for="item in items">
+                    <v-row
+                        v-if="item.heading"
+                        :key="item.heading"
+                        align="center"
+                    >
+                        <v-col cols="6">
+                            <v-subheader v-if="item.heading">
+                                {{ item.heading }}
+                            </v-subheader>
+                        </v-col>
+                        <v-col
+                            cols="6"
+                            class="text-center"
+                        >
+                            <a
+                                href="#!"
+                                class="body-2 black--text"
+                            >EDIT</a>
+                        </v-col>
+                    </v-row>
+                    <v-list-group
+                        v-else-if="item.children"
+                        :key="item.text"
+                        v-model="item.model"
+                        :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                        append-icon=""
+                    >
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{ item.text }}
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </template>
+                        <v-list-item
+                            v-for="(child, i) in item.children"
+                            :key="i"
+                            link
+                        >
+                            <v-list-item-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{ child.text }}
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-group>
+                    <v-list-item
+                        v-else
+                        :key="item.text"
+                        link
+                    >
+                        <v-list-item-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                {{ item.text }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+            :clipped-left="$vuetify.breakpoint.lgAndUp"
+            app
+            color="blue darken-3"
+            dark
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+            <v-toolbar-title
+                style="width: 300px"
+                class="ml-0 pl-4"
+            >
+                <span class="hidden-sm-and-down">Chaco Solutions</span>
+            </v-toolbar-title>
+            <v-spacer />
+            <v-btn icon>
+                <v-icon>mdi-apps</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+
+
+        <v-content>
+            <v-container
+                class="fill-height"
+                fluid
+            >
+                <v-row
+                    align="center"
+                    justify="center"
+                >
+                    <v-tooltip right>
+                        <template v-slot:activator="{ on }">
+                            <v-btn
+                                :href="source"
+                                icon
+                                large
+                                target="_blank"
+                                v-on="on"
+                            >
+                                <v-icon large>mdi-code-tags</v-icon>
+                            </v-btn>
+                        </template>
+                        <span>Source</span>
+                    </v-tooltip>
+                    <v-tooltip right>
+                        <template v-slot:activator="{ on }">
+                            <v-btn
+                                icon
+                                large
+                                href="https://codepen.io/johnjleider/pen/MNYLdL"
+                                target="_blank"
+                                v-on="on"
+                            >
+                                <v-icon large>mdi-codepen</v-icon>
+                            </v-btn>
+                        </template>
+                        <span>Codepen</span>
+                    </v-tooltip>
+                </v-row>
+            </v-container>
+        </v-content>
+
+    </v-app>
+</template>
+
+<script>
+    export default {
+        props: {
+            source: String,
+        },
+        data: () => ({
+            dialog: false,
+            drawer: null,
+            items: [
+                { icon: 'mdi-history', text: 'Trabajos' },
+                { icon: 'mdi-content-copy', text: 'Insumos' },
+                { icon: 'mdi-contacts', text: 'Proveedores' },
+                { icon: 'mdi-content-copy', text: 'Clientes' },
+                { icon: 'mdi-settings', text: 'Configuraciones' }
+                ]
+        }),
+    }
+</script>
